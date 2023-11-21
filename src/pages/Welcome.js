6fdda1fb591e14a7e39b417bd99f0e877d6e4471
Foxxo Dev <gammert54@gmail.com>
2023-11-21 20:41:55 +0100

    Bug Fixes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -17,21 +17,28 @@ const Welcome = () => {
         const res = await getData();
         setResponseData(res);
         console.log(
-          '%cBEFORE CHECKING RESPONSE \n',
+          '%cBEFORE CHECKING RESPONSE ',
           'color: red; font-size: 15px;'
         );
         console.log(res);
-        console.log('%cPLANES LIST: \n', 'color: red; font-size: 15px;');
+        console.log('%cPLANES LIST: ', 'color: red; font-size: 15px;');
         console.log(res?.planes);
 
-        if (!res || res === undefined || res === null) {
+        if (
+          !res ||
+          res === undefined ||
+          res === null ||
+          Object.keys(res).length !== 0
+        ) {
           console.log('No data saved, saving initial data');
+          console.log('Initial data:', initData(name, id));
           saveInitialData(initData(name, id));
 
           const newData = await getData();
           setResponseData(newData);
 
-          console.log('RES after init' + newData);
+          console.log('%cRES after init', 'color: red; font-size: 15px;');
+          console.log(responseData);
         }
       } catch (err) {
         alert('Application Exited With Error: ' + err);
@@ -45,27 +52,39 @@ const Welcome = () => {
     <div>
       {name} <br /> account: {responseData?.money}$ lvl {responseData?.lvl}{' '}
       <br />
-      <div className='plane_container'>
-        {responseData?.planes.map((plane) => (
-          <div className='plane_info_container' key={plane.id}>
+      {responseData && responseData.planes ? (
+        <div className='plane_container'>
+          {responseData.planes.map((plane) => (
             <div className='plane_info_container' key={plane.id}>
-              <span className='plane_info_name'>{plane.name}</span> <br />
-              <span className='plane_info_id'>{plane.id}</span> <br />
-              <span className='plane_info_amount'>{plane.amount}</span> <br />
-              <span className='plane_info_price'>{plane.price}$</span> <br />
-              <span className='plane_info_speed'>{plane.speed}KTS</span> <br />
-              <span className='plane_info_range'>{plane.range}NM</span> <br />
-              <span className='plane_info_capacity'>
-                {plane.capacity}pax
-              </span>{' '}
-              <br />
-              <span className='plane_info_fuel'>
-                {plane.fuel ? 0 : 'no fuel'}T
-              </span>
+              <div className='plane_info_container' key={plane.id}>
+                <div className='plane_info_container' key={plane.id}>
+                  <span className='plane_info_name'>{plane.name}</span> <br />
+                  <span className='plane_info_id'>{plane.id}</span> <br />
+                  <span className='plane_info_amount'>{plane.amount}</span>{' '}
+                  <br />
+                  <span className='plane_info_price'>{plane.price}$</span>{' '}
+                  <br />
+                  <span className='plane_info_speed'>
+                    {plane.speed}KTS
+                  </span>{' '}
+                  <br />
+                  <span className='plane_info_range'>{plane.range}NM</span>{' '}
+                  <br />
+                  <span className='plane_info_capacity'>
+                    {plane.capacity}pax
+                  </span>{' '}
+                  <br />
+                  <span className='plane_info_fuel'>
+                    {plane.fuel ? 0 : 'no fuel'}T
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
