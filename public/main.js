e4d75e6c120a096c760c4dc892aa5e17fd5f4986
Foxxo Dev <gammert54@gmail.com>
2023-11-23 07:38:28 +0100

    added icon

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, nativeImage } = require('electron');
+const path = require('path');
 
 require('@electron/remote/main').initialize();
 
@@ -10,14 +11,25 @@ function createWindow() {
   });
 
   win.loadURL('http://localhost:3000/');
+
+  // Set taskbar icon (for Windows and Linux)
+  const iconPath = path.join(__dirname, 'ico.png');
+  win.setIcon(iconPath);
 }
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow();
+});
+
+// Set dock name
+const image = nativeImage.createFromPath(path.join(__dirname, 'ico.png'));
+app.dock.setIcon(image);
+app.setName('Airlination');
