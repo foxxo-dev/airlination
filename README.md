ae5b28ccd8ab2ea7156a689618e8961e0af1293e
Gammert54 <96523965+Gammert54@users.noreply.github.com>
2023-11-26 18:57:28 +0100

    Bug Fixes

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ npm i
 npm run na:s (for no app starting)
 npm run s:s (for an app to open)
 ```
+
+
+# Logs
+
+## Version 1.0.0
+
+*NOT RELEASED*
--- public/main.js
+++ public/main.js
@@ -1,28 +1,25 @@
-const { app, BrowserWindow, Menu, nativeImage } = require('electron');
+const { app, BrowserWindow, nativeImage } = require('electron');
 const path = require('path');
 const isDev = require('electron-is-dev');
 const { exec } = require('child_process');
 
 require('@electron/remote/main').initialize();
 
-let mainWindow; // Declare a variable to hold the main window instance
+let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1800,
     height: 1600,
     webPreferences: { nodeIntegration: true, enableRemoteModule: true },
-    icon: './ico.png'
+    icon: path.join(__dirname, 'ico.png')
   });
 
   mainWindow.loadURL(
-    isDev ? 'http://localhost:3000/' : `file://'./index.html'`
+    isDev
+      ? 'http://localhost:3000/'
+      : `file://${path.join(__dirname, 'index.html')}`
   );
-
-  // Set taskbar icon (for Windows and Linux)
-  const iconPath = path.join(__dirname, 'ico.png');
-  mainWindow.setIcon(iconPath);
-  // exec(`wmctrl -r "Airlination" -b add,above`);
 }
 
 app.on('window-all-closed', () => {
@@ -30,17 +27,62 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  if (isDev) exec(`node ${path.join(__dirname, '/server/fileSystem.js')}`);
-  else exec(`node './server/fileSystem.js'`);
-  if (!mainWindow) createWindow(); // Create window only if it doesn't exist
+  if (isDev) {
+    exec(
+      `node ${path.join(__dirname, '/server/fileSystem.js')}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error starting server: ${error.message}`);
+          return;
+        }
+        console.log(`Server started: ${stdout}`);
+        createWindow();
+      }
+    );
+  } else {
+    exec(
+      `node ${path.join(__dirname, '/server/fileSystem.js')}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error starting server: ${error.message}`);
+          return;
+        }
+        console.log(`Server started: ${stdout}`);
+        createWindow();
+      }
+    );
+  }
 });
 
 app.on('ready', () => {
-  if (isDev) exec(`node ${path.join(__dirname, '/server/fileSystem.js')}`);
-  else exec(`node './server/fileSystem.js'`);
-  createWindow();
+  if (isDev) {
+    exec(
+      `node ${path.join(__dirname, '/server/fileSystem.js')}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error starting server: ${error.message}`);
+          return;
+        }
+        console.log(`Server started: ${stdout}`);
+        createWindow();
+      }
+    );
+  } else {
+    exec(
+      `node ${path.join(__dirname, '/server/fileSystem.js')}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error starting server: ${error.message}`);
+          return;
+        }
+        console.log(`Server started: ${stdout}`);
+        createWindow();
+      }
+    );
+  }
+
   if (process.platform === 'darwin') {
-    const image = nativeImage.createFromPath('./ico.png');
+    const image = nativeImage.createFromPath(path.join(__dirname, 'ico.png'));
     app.dock.setIcon(image);
     app.setName('Airlination');
   }
