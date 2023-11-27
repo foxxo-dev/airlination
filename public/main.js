f53563a6c94f1a9eb4f778880d3cf1d4b3c11a43
Gammert54 <96523965+Gammert54@users.noreply.github.com>
2023-11-27 19:08:24 +0100

    isDev fixes for `npm start`

--- public/main.js
+++ public/main.js
@@ -28,17 +28,7 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (isDev) {
-    exec(
-      `node ${path.join(__dirname, '/server/fileSystem.js')}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error starting server: ${error.message}`);
-          return;
-        }
-        console.log(`Server started: ${stdout}`);
-        createWindow();
-      }
-    );
+    createWindow();
   } else {
     exec(
       `node ${path.join(__dirname, '/server/fileSystem.js')}`,
@@ -55,31 +45,23 @@ app.on('activate', () => {
 });
 
 app.on('ready', () => {
-  if (isDev) {
-    exec(
-      `node ${path.join(__dirname, '/server/fileSystem.js')}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error starting server: ${error.message}`);
-          return;
-        }
-        console.log(`Server started: ${stdout}`);
+  if (isDev)
+    if (isDev)
+      if (isDev) {
         createWindow();
+      } else {
+        exec(
+          `node ${path.join(__dirname, '/server/fileSystem.js')}`,
+          (error, stdout, stderr) => {
+            if (error) {
+              console.error(`Error starting server: ${error.message}`);
+              return;
+            }
+            console.log(`Server started: ${stdout}`);
+            createWindow();
+          }
+        );
       }
-    );
-  } else {
-    exec(
-      `node ${path.join(__dirname, '/server/fileSystem.js')}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error starting server: ${error.message}`);
-          return;
-        }
-        console.log(`Server started: ${stdout}`);
-        createWindow();
-      }
-    );
-  }
 
   if (process.platform === 'darwin') {
     const image = nativeImage.createFromPath(path.join(__dirname, 'ico.png'));
